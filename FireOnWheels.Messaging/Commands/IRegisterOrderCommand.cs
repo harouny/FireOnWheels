1fbd80f651e16e4042744bae1bcd9d9eabdca959
Ahmed Elharouny <ahmedelharouny@outlook.com>
2016-03-31 21:43:51 +1100

    add RegisterOrderCommand

--- FireOnWheels.Messaging/Commands/IRegisterOrderCommand.cs
+++ FireOnWheels.Messaging/Commands/IRegisterOrderCommand.cs
@@ -2,5 +2,13 @@
 {
     public interface IRegisterOrderCommand : ICommand
     {
+        string PickupName { get; }
+        string PickupAddress { get; }
+        string PickupCity { get; }
+
+        string DeliveryName { get; }
+        string DeliveryAddress { get; }
+        string DeliveryCity { get; set; }
+
     }
 }
---
+++ FireOnWheels.Registration.Service/Messages/Commands/RegisterOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using FireOnWheels.Messaging.Commands;
+
+namespace FireOnWheels.Registration.Service.Messages.Commands
+{
+    //TODO: make it immutable
+    public class RegisterOrderCommand : IRegisterOrderCommand
+    {
+        public string PickupName { get; set; }
+        public string PickupAddress { get; set; }
+        public string PickupCity { get; set; }
+        public string DeliveryName { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string DeliveryCity { get; set; }
+    }
+}
